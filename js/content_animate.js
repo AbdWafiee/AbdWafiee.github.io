5f26c4a91061992fe4d532be1a33dc5320616bf6
AbdWafiee <a.wafiee@gmail.com>
2017-09-21 20:42:28 +0800

    updated files

--- js/content_animate.js
+++ js/content_animate.js
@@ -1,29 +1,26 @@
-$(document).ready(function() {
+//Smooth scrolling
+var $root = $("html, body");
+$(".navbar a, .titleDiv a, .education a").on('click', function(e) {
+  if (this.hash !== "") {
 
-  //Smooth scrolling
-  var $root = $("html, body");
-  $(".navbar a, .titleDiv a, .education a").on('click', function(e) {
-    if (this.hash !== "") {
-
-      //prevent default actions
-      e.preventDefault();
-
-      //cache the current section
-      var hash = this.hash;
-
-      //scroll to the referenced section
-      $root.animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function() {
-          //removes anchor from url
-          window.location.hash = "";
-        }
-      );
-    }
-  });
-
-  $(".icon").on("click", function(e) {
-    $(this).children(".stats").slideToggle(500);
+    //prevent default actions
     e.preventDefault();
-  });
+
+    //cache the current section
+    var hash = this.hash;
+
+    //scroll to the referenced section
+    $root.animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, function() {
+        //removes anchor from url
+        window.location.hash = "";
+      }
+    );
+  }
+});
+
+$(".icon").on("click", function(e) {
+  $(this).children(".stats").slideToggle(300);
+  e.preventDefault();
 });
